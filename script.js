331238ee260b8f7103055fde288f91e6df9d3f66
fpatry9 <fpatry9@gmail.com>
2026-08-02 20:13:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 window.addEventListener("scroll", function () {
     const logo = document.querySelector(".logo-principal");
+    const header = document.querySelector("header");
 
     if (window.scrollY > 100) {
         logo.style.opacity = "0";
-logo.style.transform = "scale(0)";
-        logo.style.transition = "0.4s";
+        logo.style.maxHeight = "0";
+        logo.style.margin = "0";
+        logo.style.transition = "all 0.4s ease";
+
+        header.style.paddingTop = "10px";
+        header.style.paddingBottom = "10px";
     } else {
         logo.style.opacity = "1";
-logo.style.transform = "scale(1)";
-        logo.style.transition = "0.4s";
+        logo.style.maxHeight = "450px";
+        logo.style.margin = "15px auto";
+        logo.style.transition = "all 0.4s ease";
+
+        header.style.paddingTop = "";
+        header.style.paddingBottom = "";
     }
 });
