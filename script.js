879d630012fed0fb308d9fd9b6a47d6798dd1597
fpatry9 <fpatry9@gmail.com>
2026-08-02 20:11:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ window.addEventListener("scroll", function () {
 
     if (window.scrollY > 100) {
         logo.style.opacity = "0";
-        logo.style.height = "0";
-        logo.style.margin = "0";
+logo.style.transform = "scale(0)";
         logo.style.transition = "0.4s";
     } else {
         logo.style.opacity = "1";
-        logo.style.height = "auto";
-        logo.style.margin = "15px auto";
+logo.style.transform = "scale(1)";
         logo.style.transition = "0.4s";
     }
 });
